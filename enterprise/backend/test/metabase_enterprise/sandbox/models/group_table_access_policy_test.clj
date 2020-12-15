4ad096ad285308d7aaf25dbfa4f9b66020ef6860
Cam Saul <github@camsaul.com>
2020-12-15 12:52:12 -0800

    Convert models.GTAP test namespace to new style

--- enterprise/backend/test/metabase_enterprise/sandbox/models/group_table_access_policy_test.clj
+++ enterprise/backend/test/metabase_enterprise/sandbox/models/group_table_access_policy_test.clj
@@ -1,29 +1,28 @@
 (ns metabase-enterprise.sandbox.models.group-table-access-policy-test
-  (:require [expectations :refer [expect]]
+  (:require [clojure.test :refer :all]
+            [metabase
+             [test :as mt]
+             [util :as u]]
             [metabase-enterprise.sandbox.models.group-table-access-policy :refer [GroupTableAccessPolicy]]
             [metabase.models.permissions-group :as group]
-            [metabase.test.data :as data]
-            [metabase.util :as u]
-            [toucan.db :as db]
-            [toucan.util.test :as tt]))
+            [toucan.db :as db]))
 
-;; make sure attribute-remappings come back from the DB normalized the way we'd expect
-(expect
-  {"venue_id" {:type   :category
-               :target [:variable [:field-id (data/id :venues :id)]]
-               :value  5}}
-  (tt/with-temp GroupTableAccessPolicy [gtap {:table_id             (data/id :venues)
-                                              :group_id             (u/get-id (group/all-users))
-                                              :attribute_remappings {"venue_id" {:type   "category"
-                                                                                 :target ["variable" ["field-id" (data/id :venues :id)]]
-                                                                                 :value  5}}}]
-    (db/select-one-field :attribute_remappings GroupTableAccessPolicy :id (u/get-id gtap))))
+(deftest normalize-attribute-remappings-test
+  (testing "make sure attribute-remappings come back from the DB normalized the way we'd expect"
+    (mt/with-temp GroupTableAccessPolicy [gtap {:table_id             (mt/id :venues)
+                                                :group_id             (u/get-id (group/all-users))
+                                                :attribute_remappings {"venue_id" {:type   "category"
+                                                                                   :target ["variable" ["field-id" (mt/id :venues :id)]]
+                                                                                   :value  5}}}]
+      (is (= {"venue_id" {:type   :category
+                          :target [:variable [:field-id (mt/id :venues :id)]]
+                          :value  5}}
+             (db/select-one-field :attribute_remappings GroupTableAccessPolicy :id (u/get-id gtap)))))
 
-;; apparently sometimes they are saved with just the target, but not type or value? Make sure these get normalized
-;; correctly.
-(expect
-  {"user" [:variable [:field-id (data/id :venues :id)]]}
-  (tt/with-temp GroupTableAccessPolicy [gtap {:table_id             (data/id :venues)
-                                              :group_id             (u/get-id (group/all-users))
-                                              :attribute_remappings {"user" ["variable" ["field-id" (data/id :venues :id)]]}}]
-    (db/select-one-field :attribute_remappings GroupTableAccessPolicy :id (u/get-id gtap))))
+    (testing (str "apparently sometimes they are saved with just the target, but not type or value? Make sure these "
+                  "get normalized correctly.")
+      (mt/with-temp GroupTableAccessPolicy [gtap {:table_id             (mt/id :venues)
+                                                  :group_id             (u/get-id (group/all-users))
+                                                  :attribute_remappings {"user" ["variable" ["field-id" (mt/id :venues :id)]]}}]
+        (is (= {"user" [:variable [:field-id (mt/id :venues :id)]]}
+               (db/select-one-field :attribute_remappings GroupTableAccessPolicy :id (u/get-id gtap))))))))
